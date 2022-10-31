dc5193ef4707714cb30f3b9346425cd20184f65c
Mauricio Latorre <latorre.mauricio.1992@gmail.com>
2022-10-31 17:50:30 -0300

    mergear

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Componentes
+import { ListadoMascotaComponent } from './components/listado-mascota/listado-mascota.component';
+import { AgregarEditarMascotaComponent } from './components/agregar-editar-mascota/agregar-editar-mascota.component';
+import { VerMascotaComponent } from './components/ver-mascota/ver-mascota.component';
+
+const routes: Routes = [
+  { path:'', redirectTo: 'listado-mascotas', pathMatch:'full'},
+  { path:'listado-mascotas', component: ListadoMascotaComponent},
+  { path:'agregar-mascota', component: AgregarEditarMascotaComponent},
+  { path:'ver-mascota/:id', component: VerMascotaComponent},
+  { path:'editar-mascota/:id', component: AgregarEditarMascotaComponent},
+  { path:'**', redirectTo: 'listado-mascotas', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
